e0f212039fa3ea275ee14271d1ec2998fe6e9c06
Sofia Purtova <sofiapurtova@yandex.ru>
2025-09-20 12:42:55 +0300

    3.2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,8 +22,8 @@ const app = express()
 app.use(cors({
   origin: ['http://localhost', 'http://localhost:5173'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'X-CSRF-Token']
+  //methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //allowedHeaders: ['Content-Type', 'X-CSRF-Token']
 }));
 app.use(helmet());
 app.use(cookieParser());
