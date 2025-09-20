97078254aca5a396e87ab53138648a47ddbb817d
Sofia Purtova <sofiapurtova@yandex.ru>
2025-09-20 12:35:23 +0300

    3.1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,7 +23,7 @@ app.use(cors({
   origin: ['http://localhost', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'X-CSRF-Token', 'Authorization']
+  allowedHeaders: ['Content-Type', 'X-CSRF-Token']
 }));
 app.use(helmet());
 app.use(cookieParser());
